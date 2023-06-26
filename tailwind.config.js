08eb68555e8d916f7203b30842dc3baff1d3956f
Reydem <reydem613@gmail.com>
2023-06-26 12:57:05 -0500

    23. Animando el logo - Parte 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,21 @@ module.exports = {
     extend: {
       backgroundImage: theme => ({
         'body-pattern': "url('../img/pattern.png')",
-       }),
-       colors: {
+      }),
+      colors: {
         "azul-claro": "#37bcf9",
         "azul-oscuro": "#0370b9"
-    }
+      },
+      animation: {
+        'spin-low': 'spin 2s linear infinite',
+        'from-bellow': 'fromBellow 500ms linear',
+        'from-right': 'fromRight 300ms linear',
+        'bg-banner': 'backBanner 10s linear',
+        'text-banner': 'showBannerText 10s linear',
+        'show-card-icon': 'showCardIcon 300ms linear',
+        'show-card-category': 'showCardCategory 400ms linear',
+        'show-card-desc': 'showCardDesc 400ms linear'
+      },
     },
   },
   variants: {
