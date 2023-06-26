5b3098820615f00f7554d5600ba914e3c4f05b89
Reydem <reydem613@gmail.com>
2023-06-26 13:24:04 -0500

    25. Animación avanzada en el logo - Parte 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      screens: {
+        'xs': { 'max': '768px' }
+      },
+      boxShadow: {
+        header3D: "0px 1px 0px #393d3f, 1px 2px 0px #393d3f, 2px 3px 0px #393d3f, 3px 4px 0px #393d3f",
+        box: "0px 0px 1px rgba(0, 0, 0, 0.3), 0px 3px 7px rgba(0, 0, 0, 0.3), 0px 1px white inset, 0px -3px 1px rgba(0, 0, 0, 0.3) inset"
+      },
       backgroundImage: theme => ({
         'body-pattern': "url('../img/pattern.png')",
+        'banner': "url('../img/bakbaner.png')"
       }),
+      backgroundPosition: {
+        'banner-position': "-200px -200px"
+      },
       colors: {
         "azul-claro": "#37bcf9",
         "azul-oscuro": "#0370b9"
@@ -14,16 +23,28 @@ module.exports = {
         'spin-low': 'spin 2s linear infinite',
         'from-bellow': 'fromBellow 500ms linear',
         'from-right': 'fromRight 300ms linear',
-        'bg-banner': 'backBanner 10s linear',
-        'text-banner': 'showBannerText 10s linear',
-        'show-card-icon': 'showCardIcon 300ms linear',
-        'show-card-category': 'showCardCategory 400ms linear',
-        'show-card-desc': 'showCardDesc 400ms linear'
+
       },
-    },
+      keyframes: {
+        fromBellow: {
+          '0%': { transfrom: 'translateY(0%)' },
+          '50%': { transform: 'translateY(200%)' },
+          '100%': { transfrom: 'translateY(0%)' }
+        },
+
+        fromRight: {
+          '0%': { transform: 'translateX(200%)' },
+          '100%': { transform: 'translateX(0%)' }
+        },
+      }
+    }
   },
   variants: {
-    extend: {},
-  },
-  plugins: [],
+    animation: ['responsive', 'hover', 'group-hover'],
+    animate: ['responsive', 'hover', 'group-hover'],
+    fontSize: ['responsive', 'hover', 'group-hover'],
+    transform: ['responsive', 'hover', 'group-hover'],
+    scale: ['responsive', 'hover', 'group-hover'],
+    padding: ['responsive', 'hover', 'group-hover'],
+  }
 }
