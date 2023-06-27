179930b74f226a5aeac52a0cdf57cccf18933c86
Reydem <reydem613@gmail.com>
2023-06-27 06:46:24 -0500

    32. Animando el texto del banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
         'from-bellow': 'fromBellow 500ms linear',
         'from-right': 'fromRight 300ms linear',
         'bg-banner': 'backBanner 10s linear',
+        'text-banner': 'showBannerText 10s linear',
 
       },
       keyframes: {
@@ -38,9 +39,31 @@ module.exports = {
           '100%': { transform: 'translateX(0%)' }
         },
         backBanner: {
-          '0%': { 'background-position': '0px 0px'},
-          '100%': { 'background-position': '-200px -200px'}
-      },
+          '0%': { 'background-position': '0px 0px' },
+          '100%': { 'background-position': '-200px -200px' }
+        },
+        showBannerText: {
+          '0%': {
+            transform: "translateX(-600%) scale(5,5)",
+            opacity: 1
+          },
+          '50%': {
+            transform: "translateX(600%) scale(5,5)",
+            opacity: 1
+          },
+          '75%': {
+            transform: 'translateX(-600%) scale(5,5)',
+            'text-shadow': 'none',
+            color: 'transparent',
+            opacity: 0
+          },
+          '100%': {
+            transform: 'translateX(0%) scale(1,1)',
+            'text-shadow': '2px 2px 1px black',
+            color: 'white',
+            opacity: 1
+          }
+        },
       }
     }
   },
